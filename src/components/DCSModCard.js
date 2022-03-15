59837a3da4c263c550fa2586d7a8b63a70e7a951
Pinsho <pinsho@gmail.com>
2022-03-16 00:42:47 +0100

    Initial release

--- src/components/DCSModCard.js
+++ src/components/DCSModCard.js
@@ -6,11 +6,11 @@ export const DCSModCard = ({ mod }) => {
     return (
         <li className={styles.modCard}>
             <img src={mod.fields.Picture} width={300} height={165} alt={mod.fields.title} className={styles.modImage}/>
-            <div className={styles.info}><a href={mod.fields.Info} target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faCircleInfo}/></a></div>
+            <div className={styles.info}><a href={mod.fields.Info} target="_blank" ><FontAwesomeIcon icon={faCircleInfo}/></a></div>
             <div className={styles.cardBody}>
                 <div className={styles.title}>{mod.fields.Unit}</div>
                 <div className={styles.type}><i className='bx bx-info-circle' ></i>{mod.fields.Type}</div>
-                <div className={styles.downloadButton}><a href={mod.fields.URL} target="_blank" rel="noreferrer">Download</a></div>
+                <div className={styles.downloadButton}><a href={mod.fields.URL} target="_blank" >Download</a></div>
             </div>
         </li>
     );
