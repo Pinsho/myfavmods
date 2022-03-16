efe2babc3abd88abc5736548e4bc446be70e644c
Pinsho <pinsho@gmail.com>
2022-03-16 23:23:53 +0100

    CSS Home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -13,22 +13,22 @@ export default class Contact extends Component {
                 <ul className='homeGrid'>
                     <div className='wrapper'>
                         <Link to="/myfavmods/dcs"><img src={logodcs} height={64} alt="DCS World"/></Link>
-                        <span><Link to="/myfavmods/dcs">Digital Combat Simulator World</Link></span>
+                        <span className='long'><Link to="/myfavmods/dcs">Digital Combat Simulator World</Link></span>
                     </div>
                     <div className='wrapper'>
                         <Link to="/myfavmods/msfs2020"><img src={logomsfs} height={64} alt="Microsoft Flight Simulator 2020"/></Link>
-                        <span><Link to="/myfavmods/msfs2020">Microsoft Flight Simulator 2020</Link></span>
+                        <span className='long'><Link to="/myfavmods/msfs2020">Microsoft Flight Simulator 2020</Link></span>
                     </div>
                 </ul>
+                
                 <div className='homeSection'><i className='bx bxs-car' ></i>Motor Simulation</div>
                 <div className='homeSection-tab'></div>
                 <ul className='homeGrid'>
                     <div className='wrapper'>
                         <Link to="/myfavmods/assettocorsa"><img src={logoac} height={64} alt="Assetto Corsa"/></Link>
-                        <span><Link to="/myfavmods/assettocorsa">Assetto Corsa</Link></span>
+                        <span className='long'><Link to="/myfavmods/assettocorsa">Assetto Corsa</Link></span>
                     </div>
                 </ul>
-                
             </div>
         )
     }
