e8bfdce71f8cd91b53d4b6b0e0f33c1b1290ff04
Pinsho <pinsho@gmail.com>
2022-03-16 08:22:17 +0100

    Initial release

--- src/components/NavbarComp.js
+++ src/components/NavbarComp.js
@@ -20,7 +20,7 @@ export default class NavbarComp extends Component {
             <Router>
                 <div>
                     <Navbar bg="dark" variant={"dark"} expand="lg">
-                        <Navbar.Brand as={Link} to="/"><i className='bx bx-extension logo'></i><span className='logoText'>myfavmods</span></Navbar.Brand>
+                        <Navbar.Brand as={Link} to="/myfavmods"><i className='bx bx-extension logo'></i><span className='logoText'>myfavmods</span></Navbar.Brand>
                         <Navbar.Toggle aria-controls="navbarScroll" />
                         <Navbar.Collapse id="navbarScroll">
                             <Nav
@@ -28,7 +28,7 @@ export default class NavbarComp extends Component {
                                 style={{ maxHeight: '100%' }}
                                 navbarScroll
                             >
-                                <Nav.Link as={Link} to="/">Home</Nav.Link>
+                                <Nav.Link as={Link} to="/myfavmods">Home</Nav.Link>
                                 <NavDropdown title="Flight Sim" id="basic-nav-dropdown">
                                 <NavDropdown.Item as={Link} to="/dcs">DCS World</NavDropdown.Item>
                                 <NavDropdown.Item as={Link} to="/msfs2020">MSFS 2020</NavDropdown.Item>
@@ -47,7 +47,7 @@ export default class NavbarComp extends Component {
                 </div>
                 <div>
                     <Routes>
-                        <Route path="/" element={<Home/>}/>
+                        <Route path="/myfavmods" element={<Home/>}/>
                         <Route path="/dcs" element={<DCS/>}/>
                         <Route path="/msfs2020" element={<MSFS2020/>}/>
                         <Route path="/rfactor2" element={<Rfactor2/>}/>
