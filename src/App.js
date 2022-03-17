deae4ee68a58b41b7aaf5119ab75c769c3dd54a4
Pinsho <pinsho@gmail.com>
2022-03-17 11:01:44 +0100

    Added back-to-top button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-
 import NavbarComp from './components/NavbarComp';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Footer from './Footer';
-import MSFS2020 from './components/pages/MSFS2020';
+import './index.css';
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 300) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    });
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // for smoothly scrolling
+    });
+  };
+
   return (
     <div className='content'> 
       <header>
           <NavbarComp />
       </header>
       <main>
+        {showButton && (
+          <button onClick={scrollToTop} className="back-to-top">
+            &#8679;
+          </button>
+        )}
       </main>
     </div>
   );
