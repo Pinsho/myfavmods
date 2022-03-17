b25d99731699d9501a4a087606fb9528862916dc
Pinsho <pinsho@gmail.com>
2022-03-17 14:46:35 +0100

    Footer fix

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ export default class Contact extends Component {
         return (
             <div className='footer'>
                 <div className='footerText'>
-                    <span><i className='bx bxl-github' ></i>myfavmods by pinsho</span>
+                    <span><i className='bx bxl-github' ></i>myfavmods by pinsho </span>
                     <span className='socials'>
                         <a href="https://instagram.com/pinsho" target="_blank" rel="noreferrer">
                             <i className='bx bxl-instagram instagram'></i>
