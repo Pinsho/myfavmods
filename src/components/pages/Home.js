f45bfb3943b8195ae19f9dfae736f718c2e86c3c
Pinsho <pinsho@gmail.com>
2022-03-18 18:43:39 +0100

    More grid css fixes

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -8,27 +8,30 @@ export default class Contact extends Component {
     render() {
         return (
             <div className='home'>
-                <div className='homeSection'><i className='bx bxs-plane-alt'></i>FLIGHT SIMULATION</div>
-                {/* <div className='homeSection-tab'></div> */}
-                <ul className='homeGrid'>
-                    <div className='wrapper'>
-                        <Link to="/myfavmods/dcs"><img src={logodcs} height={64} alt="DCS World"/></Link>
-                        <span className='long'><Link to="/myfavmods/dcs">Digital Combat Simulator World</Link></span>
+                <div className='category'>
+                    <div className='gridContainer'>
+                        <div className='homeSection'><i className='bx bxs-plane-alt'></i><span>FLIGHT SIM GAMES</span></div>
+                        <ul className='homeGrid'>
+                            <div className='wrapper'>
+                                <Link to="/myfavmods/dcs"><img src={logodcs} height={64} alt="DCS World"/></Link>
+                                <span className='long'><Link to="/myfavmods/dcs">Digital Combat Simulator World</Link></span>
+                            </div>
+                            <div className='wrapper'>
+                                <Link to="/myfavmods/msfs2020"><img src={logomsfs} height={64} alt="Microsoft Flight Simulator 2020"/></Link>
+                                <span className='long'><Link to="/myfavmods/msfs2020">Microsoft Flight Simulator</Link></span>
+                            </div>
+                        </ul>
                     </div>
-                    <div className='wrapper'>
-                        <Link to="/myfavmods/msfs2020"><img src={logomsfs} height={64} alt="Microsoft Flight Simulator 2020"/></Link>
-                        <span className='long'><Link to="/myfavmods/msfs2020">Microsoft Flight Simulator</Link></span>
+                    <div className='gridContainer'>
+                        <div className='homeSection'><i className='bx bxs-car' ></i><span>MOTOR SIM GAMES</span></div>
+                        <ul className='homeGrid'>
+                            <div className='wrapper'>
+                                <Link to="/myfavmods/assettocorsa"><img src={logoac} height={64} alt="Assetto Corsa"/></Link>
+                                <span className='long'><Link to="/myfavmods/assettocorsa">Assetto Corsa</Link></span>
+                            </div>
+                        </ul>
                     </div>
-                </ul>
-                
-                <div className='homeSection'><i className='bx bxs-car' ></i>MOTOR SIMULATION</div>
-                {/* <div className='homeSection-tab'></div> */}
-                <ul className='homeGrid'>
-                    <div className='wrapper'>
-                        <Link to="/myfavmods/assettocorsa"><img src={logoac} height={64} alt="Assetto Corsa"/></Link>
-                        <span className='long'><Link to="/myfavmods/assettocorsa">Assetto Corsa</Link></span>
-                    </div>
-                </ul>
+                </div>
             </div>
         )
     }
