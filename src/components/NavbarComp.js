56ff16e794dd5bcb081cc1171cd859166cb4c88c
Pinsho <pinsho@gmail.com>
2022-03-21 11:00:03 +0100

    Fixing Router on refresh

--- src/components/NavbarComp.js
+++ src/components/NavbarComp.js
@@ -18,7 +18,7 @@ export default class NavbarComp extends Component {
             <Router basename="/myfavmods">
                 <div>
                     <Navbar bg="dark" variant={"dark"} expand="lg">
-                        <Navbar.Brand as={Link} to="/myfavmods/"><i className='bx bx-extension logo'></i><span className='logoText'>myfavmods</span></Navbar.Brand>
+                        <Navbar.Brand as={Link} to="/"><i className='bx bx-extension logo'></i><span className='logoText'>myfavmods</span></Navbar.Brand>
                         <Navbar.Toggle aria-controls="navbarScroll" />
                         <Navbar.Collapse id="navbarScroll">
                             <Nav
@@ -26,15 +26,15 @@ export default class NavbarComp extends Component {
                                 style={{ maxHeight: '100%' }}
                                 navbarScroll
                             >
-                                <Nav.Link as={Link} to="/myfavmods/">Home</Nav.Link>
+                                <Nav.Link as={Link} to="/">Home</Nav.Link>
                                 <NavDropdown title="Flight Sim" id="basic-nav-dropdown">
-                                <NavDropdown.Item as={Link} to="/myfavmods/dcs">DCS World</NavDropdown.Item>
-                                <NavDropdown.Item as={Link} to="/myfavmods/msfs2020">MSFS 2020</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to="/dcs">DCS World</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to="/msfs2020">MSFS 2020</NavDropdown.Item>
                                 {/* <NavDropdown.Divider />
                                 <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item> */}
                                 </NavDropdown>
                                 <NavDropdown title="Motor Sim" id="basic-nav-dropdown">
-                                <NavDropdown.Item as={Link} to="/myfavmods/assettocorsa">Assetto Corsa</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to="/assettocorsa">Assetto Corsa</NavDropdown.Item>
                                 {/* <NavDropdown.Item as={Link} to="/myfavmods/rfactor2">rFactor 2</NavDropdown.Item> */}
                                 </NavDropdown>
                                 {/* <Nav.Link as={Link} to="/contact">Contact</Nav.Link> */}
@@ -44,11 +44,11 @@ export default class NavbarComp extends Component {
                 </div>
                 <div>
                     <Routes>
-                        <Route path="/myfavmods/" element={<Home/>}/>
-                        <Route path="/myfavmods/dcs" element={<DCS/>}/>
-                        <Route path="/myfavmods/msfs2020" element={<MSFS2020/>}/>
-                        {/* <Route path="/myfavmods/rfactor2" element={<Rfactor2/>}/> */}
-                        <Route path="/myfavmods/assettocorsa" element={<AssettoCorsa/>}/>
+                        <Route path="/" element={<Home/>}/>
+                        <Route path="/dcs" element={<DCS/>}/>
+                        <Route path="/msfs2020" element={<MSFS2020/>}/>
+                        {/* <Route path="/rfactor2" element={<Rfactor2/>}/> */}
+                        <Route path="/assettocorsa" element={<AssettoCorsa/>}/>
                         {/* <Route path="/contact" element={<Contact/>}/> */}
                     </Routes>
                 </div>
