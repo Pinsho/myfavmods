64b760ebd84fb8431257f6b1802c12ae4bbf02e1
Pinsho <pinsho@gmail.com>
2022-04-03 10:30:41 +0200

    Fixed Type icon

--- src/components/Rfactor2OtherCard.js
+++ src/components/Rfactor2OtherCard.js
@@ -6,7 +6,7 @@ export const Rfactor2OtherCard = ({ other }) => {
             <img src={other.fields.Picture} width={300} height={165} alt={other.fields.Name} className={styles.modImage}/>
             <div className={styles.cardBody}>
                 <div className={styles.title}>{other.fields.Name}</div>
-                <div className={styles.type}><i className='bx bxs-map' ></i>Type: <div className={styles.right}>{other.fields.TypeName}</div></div>
+                <div className={styles.type}><i className='bx bxs-info-circle' ></i>Type: <div className={styles.right}>{other.fields.TypeName}</div></div>
                 <div><a href={other.fields.URL} target="_blank" rel="noreferrer" className={styles.downloadButtonRF2}>Download</a></div>
             </div>
         </li>
